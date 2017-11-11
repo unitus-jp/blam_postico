20352cf2eebf11c9ab4099c1a9abdb962c31a931
saikishunsuke <saikishunsuke0828@yahoo.co.jp>
2017-11-12 00:43:26 +0900

    add game model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111134218) do
+ActiveRecord::Schema.define(version: 20171111154013) do
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.integer "company_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
