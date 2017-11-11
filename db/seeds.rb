20f7e5f452d9e2dbc44c8d7cd5c36b752aa588e7
Takahito Yamada <d_p.t.a.d.fighter@icloud.com>
2017-11-12 01:06:15 +0900

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+100.times do |n|
+  Suggestion.create(content: n.to_s + "これはサンプルのコンテンツです。", game_id: n, user_id: n)
+end
+
+
+100.times do |n|
+  Company.create(name: "Comapy" + n.to_s)
+end
+
+100.times do |n|
+  Game.create(name: "ゲーム" + n.to_s, company_id: n)
+end
