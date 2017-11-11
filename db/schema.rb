2859911ab89409d7a55887e54a68674aa2d26015
Takahito Yamada <d_p.t.a.d.fighter@icloud.com>
2017-11-12 00:28:47 +0900

    Add model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111134218) do
+ActiveRecord::Schema.define(version: 20171111152629) do
+
+  create_table "suggestions", force: :cascade do |t|
+    t.text "content"
+    t.integer "game_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
