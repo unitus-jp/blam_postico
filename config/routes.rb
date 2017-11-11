269d4072544a7184ae876407ddbe2cac6be37150
Takahito Yamada <d_p.t.a.d.fighter@icloud.com>
2017-11-12 01:07:51 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root to: "home#index"
+
+ scope "company" do
+   get 'new'
+ end
 end
