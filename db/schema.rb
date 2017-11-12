936be249f614083f46c0ea030c5ee2e8ae9f0dfc
Miyuki Suzuki <miyuki.s.0419.8680@gmail.com>
2017-11-12 11:34:44 +0900

    :bug: fix database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111234446) do
+ActiveRecord::Schema.define(version: 20171111230712) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
